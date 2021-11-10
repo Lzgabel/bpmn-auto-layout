f07c3ac7211188de428088b581233c64593ab1f8
李銍 <lizhi@shuwen.com>
2021-11-10 13:26:51 +0800

    chore: 删除无用代码

--- src/main/java/cn/lzgabel/App.java
+++ src/main/java/cn/lzgabel/App.java
@@ -1,5 +1,6 @@
 package cn.lzgabel;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.util.HashMap;
 
@@ -21,34 +22,8 @@ public class App
 
 	public static void main( String[] args ) throws Exception
     {
-    	String filename = "";
-    	//filename = "real_diagrams/ApprovalProcess"; //-- Works!
-    	//filename = "real_diagrams/ExecuteNomineeBookings"; // -- Works!
-    	//String filename = "real_diagrams/Nachlieferung-TC8-2Nachlieferungen"; -- Works!
-    	//filename = "real_diagrams/P8CreditorTransfer(fromSIS)-Level2"; // -- Works!
-    	//filename = "real_diagrams/SISZuordnungvonDispo-Pool-Dokumenten"; // -- Works!
-    	//filename = "real_diagrams/P9"; // -- Works!
-    	//filename = "real_diagrams/P15-GBDBS21-Grundbuch-Mitteilungen_-Anzeigen"; // -- Works!
-    	//filename = "real_diagrams/P14-GBDBS21-GesuchZustimmung_Bewilligung"; // -- Works!
-    	//filename = "real_diagrams/TaxOfficeIntegration"; // -- Works!
-
-    	//filename = "real_diagrams/GBDBS21-NachlieferungExtensions"; // -- API ERROR
-    	//filename = "real_diagrams/P10OwnerExchange"; //  -- API ERROR
-
-    	filename = "A.1.0";
-
-    	for(String arg : args)
-		{
-    		if(arg.equals("-move"))
-    			move  = true;
-		}
-
-    	if(args.length > 0)
-    	{
-        	layoutFiles(args);
-    	}
-    	else
-    		layoutFile(filename);
+    	String filename = "A.1.0";
+    	layoutFile(filename);
     }
 
 	private static void layoutFiles(String[] files)
@@ -67,7 +42,6 @@ public class App
     	FileUtils.copyFile(file, copy);
 
     	HashMap<String, Element> extensionMap = Util.removeAndGetElementsFromXML(filePath + "copy", "extensionElements");
-    	//HashMap<String, Element> sedMap = Util.removeAndGetElementsFromXML(filePath + "copy", "signalEventDefinition");
 
     	BpmnModel model = Util.readBPMFile(copy);
 
@@ -83,7 +57,6 @@ public class App
     	String name = "target/" + filename + "_layout.bpmn";
     	Util.writeModel(model, name);
     	Util.addXMLElementsBackToFile(extensionMap, name);
-    	//Util.addXMLElementsBackToFile(sedMap, name);
 
     	copy.delete();
     }
